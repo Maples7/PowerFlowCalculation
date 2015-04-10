ed61809ab932ef9e073edb89fd3ae0981e2d645f
Maples7 <maples7@163.com>
2015-04-10 16:56:24 +0800

    drawing convergence graph V 1.2
    comparing with PQ decomposition method

--- PowerFlowCalculation/PowerFlowCalculation/DrawConvergenceGraph.py
+++ PowerFlowCalculation/PowerFlowCalculation/DrawConvergenceGraph.py
@@ -13,7 +13,8 @@ import pylab as pl
 
 # main
 y_down = 0
-y_up = 0.5
+y_up = 0.6
+floatResBit = 7
 
 plot = []
 pl.title('Convergence Graph')
@@ -23,11 +24,23 @@ pl.grid(True)
 pl.ylim(y_down, y_up)
 
 # draw Newton-Raphson method
-draw = pl.plot(x_axis, y_axis, label='Newton-Raphson method')
+draw = pl.plot(x_axis, y_axis, 'blue', label='Newton-Raphson Method')
 plot.append(draw)
 
 for i in range(len(x_axis)):        # show points' data text
-    pl.text(x_axis[i], y_axis[i], str(round(y_axis[i], 7)))
+    pl.text(x_axis[i], y_axis[i], str(round(y_axis[i], floatResBit)))
+
+
+# draw PQ decomposition method， The Data is from Page 115 in Reference book
+# for comparison
+x_axis = [1, 2, 3, 4, 5, 6, 7]
+y_axis = [0.55580, 0.07135, 0.00715, 0.00078, 0.00020, 0.00005, 0.00001]
+draw = pl.plot(x_axis, y_axis, 'red', label='PQ Decomposition Method')
+plot.append(draw)
+
+for i in range(len(x_axis)):        # show points' data text
+    pl.text(x_axis[i], y_axis[i], str(round(y_axis[i], floatResBit)))
+
 
 
 pl.legend()
